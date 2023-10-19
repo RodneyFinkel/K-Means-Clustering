b2059fcaadd36e041990a4996542f1d7626b3143
Rodney Finkel <111357994+RodneyFinkel@users.noreply.github.com>
2023-10-19 21:09:51 +0300

    SYNV1

---
+++ k_meansSP500.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd 
+import pandas_datareader as dr 
+import yfinance as yf 
+
+from pylab import plot, show
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+from numpy.random import rand
+from scipy.cluster.vq import kmeans, vq
+from math import sqrt
+from sklearn.cluster import KMeans
+from sklearn import preprocessing
