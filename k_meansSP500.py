e317c504e84aa2dab905e161f1879682bb182d56
Rodney Finkel <111357994+RodneyFinkel@users.noreply.github.com>
2023-10-20 19:05:47 +0300

    SYNV1

--- k_meansSP500.py
+++ k_meansSP500.py
@@ -1,6 +1,10 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+
 import numpy as np
 import pandas as pd 
-import pandas_datareader as dr 
+# import pandas_datareader as dr 
 import yfinance as yf 
 
 from pylab import plot, show
@@ -12,6 +16,9 @@ from scipy.cluster.vq import kmeans, vq
 from math import sqrt
 from sklearn.cluster import KMeans
 from sklearn import preprocessing
+
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
 
 sp500_url = 'https://en.wikipedia.org/wiki/List_of_S%26P_500_companies'
 
@@ -40,9 +47,36 @@ prices_df = pd.concat(prices_list, axis=1) # concatenate all the dataframes alon
 prices_df.sort_index(inplace=True)
 
 returns = pd.DataFrame()
+# mean of daily returns
 returns['Returns'] = prices_df.pct_change().mean() * 252 # mean of daily returns percentage, annualized
 
 # Define the column volatility
 returns["Volatility"] = prices_df.pct_change().std() * sqrt(252) # standard deviation of daily returns, annualized
 print(returns)
+
+# format data as a numpy array to feed into the K-Means algorithm
+data = np.asarray([np.asarray(returns['Returns']), np.asarray(returns['Volatility'])]).T 
+
+# if X has missing data use Iterative imputer to generate missing values based on relationship between the variables themselves
+# https://scikit-learn.org/stable/modules/generated/sklearn.impute.IterativeImputer.html#sklearn.impute.IterativeImputer
+
+imputer = IterativeImputer(max_iter=10, initial_strategy='mean', random_state=0)
+imputer.fit(data)
+data_imputed = imputer.transform(data)
+X = data_imputed
+
+WCSS = [] # within cluster sum of squares
+for k in range(2, 20):
+    k_means = KMeans(n_clusters = k) # creating an instance of the KMeans algorithm   
+    k_means.fit(X)
+    WCSS.append(k_means.inertia_)
+fig = plt.figure(figsize=(15, 5))
+
+plt.plot(range(2, 20), WCSS)
+plt.grid(True)
+plt.title('Elbow Curve')
+plt.show()
+
+
+
 
